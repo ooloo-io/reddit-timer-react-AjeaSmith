c6f5c58c445d477dc8bfb209801516ea8fea6fca
Ajea Smith <smithajea@rocketmail.com>
2021-03-12 19:21:21 -0500

    added more font styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
-const HomePage = () => (<div>Im the Home</div>);
+const HomePage = () => (
+  <main>
+    <Header />
+    <p>Im Home</p>
+  </main>
+);
 
 export default HomePage;
