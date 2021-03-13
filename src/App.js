a36029194dba8d89e04228989d9e52cb359228f0
Ajea Smith <smithajea@rocketmail.com>
2021-03-13 13:30:48 -0500

    formatted eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import SearchPage from "./pages/SearchPage";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
