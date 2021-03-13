2cacb5607de668b1cff15cc822d892f2abde3a47
Ajea Smith <smithajea@rocketmail.com>
2021-03-13 16:15:34 -0500

    created footer component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
 const HomePage = () => (
   <main>
     <Header />
-    <p>Im Home</p>
+    <Footer/>
   </main>
 );
 
