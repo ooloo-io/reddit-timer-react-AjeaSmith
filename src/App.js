4aa15986145cfac373a078d72b37eabbe8b72155
Ajea Smith <smithajea@rocketmail.com>
2021-03-12 19:40:44 -0500

    removed exact prop from router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route path="/" component={HomePage} />
         <Route path="/search" component={SearchPage} />
       </Switch>
     </Router>
