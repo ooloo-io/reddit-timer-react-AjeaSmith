01ee4291a59c6ae1fef2f7894c9c0b8f770a813c
Ajea Smith <smithajea@rocketmail.com>
2021-03-13 16:35:41 -0500

    added footer logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../footerlogo.svg';
 
-const Footer = () => {
-    return (
-        <div>
-            Im the footer
-        </div>
-    )
-}
+const Footer = () => (
+  <footer>
+    <nav>
+      <ul>
+        <li>ooloo</li>
+        <li>
+          <Link to="/">
+            <img src={logo} alt="footer" />
+          </Link>
+        </li>
+        <li>Terms & Privacy</li>
+      </ul>
+    </nav>
+  </footer>
+);
 
-export default Footer
+export default Footer;
