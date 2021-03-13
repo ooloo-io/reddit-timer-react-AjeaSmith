e5084cbca8e5880368a41ae9149aacf2d0431b2f
Ajea Smith <smithajea@rocketmail.com>
2021-03-12 21:08:57 -0500

    fixed conflict issues

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Header from '../components/Header';
 
 const HomePage = () => (
@@ -8,9 +7,5 @@ const HomePage = () => (
     <p>Im the home</p>
   </main>
 );
-=======
-
-const HomePage = () => (<div>Im the Home</div>);
->>>>>>> 35ac33d7b73b01627651566aa19ca86e8886dc75
 
 export default HomePage;
