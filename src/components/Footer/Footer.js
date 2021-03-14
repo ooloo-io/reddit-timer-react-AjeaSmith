f4c3b01a151cc1174fe3f05a155b87c0e6aa0ed9
Ajea Smith <smithajea@rocketmail.com>
2021-03-14 14:57:46 -0400

    added links to nav items

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../footerlogo.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../footerlogo.svg";
 
 const Footer = () => (
   <footer>
     <nav>
       <ul>
-        <li>ooloo</li>
+        <li>
+          <Link to="https://ooloo.io/employers">ooloo</Link>
+        </li>
         <li>
           <Link to="/">
             <img src={logo} alt="footer" />
           </Link>
         </li>
-        <li>Terms & Privacy</li>
+        <li>
+          <Link to="/terms">Terms & Privacy</Link>
+        </li>
       </ul>
     </nav>
   </footer>
