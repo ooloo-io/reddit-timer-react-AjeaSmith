6901a8488651af00e32d92853d18edee69b69917
Ajea Smith <smithajea@rocketmail.com>
2021-03-15 12:08:14 -0400

    added footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../../footerlogo.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../footerlogo.svg';
+import {
+  Wrapper, Nav, Ul, Img,
+} from './Footer.style';
 
 const Footer = () => (
-  <footer>
-    <nav>
-      <ul>
+  <Wrapper>
+    <Nav>
+      <Ul>
         <li>
           <Link to="https://ooloo.io/employers">ooloo</Link>
         </li>
         <li>
           <Link to="/">
-            <img src={logo} alt="footer" />
+            <Img src={logo} alt="footer" />
           </Link>
         </li>
         <li>
           <Link to="/terms">Terms & Privacy</Link>
         </li>
-      </ul>
-    </nav>
-  </footer>
+      </Ul>
+    </Nav>
+  </Wrapper>
 );
 
 export default Footer;
---
+++ src/components/Footer/Footer.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.footer`
+  width: 100%;
+`;
+
+export const Nav = styled.nav`
+  width: 100%;
+`;
+
+export const Ul = styled.ul`
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  list-style: none;
+`;
+
+export const Img = styled.img`
+  width: 35px;
+`;
