8bd63d854b8de4523e32cd2792ed572998a9c5fd
Ajea Smith <smithajea@rocketmail.com>
2021-03-17 17:26:25 -0400

    added title & subtitle

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div>
-            Im the hero section
-        </div>
-    )
-}
+  return (
+    <section>
+      <div className="hero-content">
+        <h2>No reactions to your reddit posts?</h2>
+        <small>
+          Great timing, great results! Find the best time to post on your
+          subreddit.
+        </small>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
