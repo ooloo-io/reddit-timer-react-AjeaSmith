7dccf7e53b0f5bbfc9c2854104f97d2417219402
Ajea Smith <smithajea@rocketmail.com>
2021-03-17 17:11:58 -0400

    added hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+            Im the hero section
+        </div>
+    )
+}
+
+export default Hero
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Hero from '../components/Hero/Hero';
 
 const HomePage = () => (
   <main>
     <p>Home Page</p>
+    <Hero/>
   </main>
 );
 
