49c066ab23d43fb27ea2c4b6b55023c07a2b7ec5
Ajea Smith <smithajea@rocketmail.com>
2021-03-18 00:41:52 -0400

    adjusted footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../footerlogo.svg';
 import {
-  Wrapper, Nav, Ul, Img,
+  Wrapper, Ul, Img, Li,
 } from './Footer.style';
 
 const Footer = () => (
   <Wrapper>
-    <Nav>
-      <Ul>
-        <li>
-          <Link to={{ pathname: 'https://ooloo.io/employers' }} target="_blank">
-            ooloo.io
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <Img src={logo} alt="footer" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/terms">Terms & Privacy</Link>
-        </li>
-      </Ul>
-    </Nav>
+    <Ul>
+      <Li>
+        <Link to={{ pathname: 'https://ooloo.io/employers' }} target="_blank">
+          ooloo.io
+        </Link>
+      </Li>
+      <Li>
+        <Link to="/">
+          <Img src={logo} alt="footer" />
+        </Link>
+      </Li>
+      <Li>
+        <Link to="/terms">Terms & Privacy</Link>
+      </Li>
+    </Ul>
   </Wrapper>
 );
 
