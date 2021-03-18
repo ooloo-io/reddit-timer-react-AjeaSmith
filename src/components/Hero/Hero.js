33409ad4dd7932877c792887c7c60d330053f439
Ajea Smith <smithajea@rocketmail.com>
2021-03-18 15:57:42 -0400

    button & image link to search page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import heatmapPNG from '../../images/heatmap.png';
 
 const Hero = () => (
@@ -9,11 +10,15 @@ const Hero = () => (
         Great timing, great results! Find the best time to post on your
         subreddit.
       </small>
-      <button type="button">SHOW ME THE BEST TIME</button>
+      <button type="button">
+        <Link to="/search/javascript">SHOW ME THE BEST TIME</Link>
+      </button>
       <small>r/javascript</small>
     </div>
     <div className="hero-image">
-      <img src={heatmapPNG} alt="heatmap" srcSet="" />
+      <Link to="/search/javascript">
+        <img src={heatmapPNG} alt="heatmap" srcSet="" />
+      </Link>
     </div>
   </section>
 );
