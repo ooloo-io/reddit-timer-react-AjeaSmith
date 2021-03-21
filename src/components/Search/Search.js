be584c92e9d8895b71c9d51016e0940bdfdae3db
Ajea Smith <smithajea@rocketmail.com>
2021-03-21 13:31:54 -0400

    added form title

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Search = () => (
-  <div>
-    Im the search component
-  </div>
+  <section>
+    <h2>Find the best time for a subreddit</h2>
+    <form></form>
+  </section>
 );
 
 export default Search;
