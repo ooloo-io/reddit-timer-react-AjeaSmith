694bf8e37a10cc0bacde78631d85a76e52841661
Ajea Smith <smithajea@rocketmail.com>
2021-03-21 16:07:34 -0400

    added form styles

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-
+import * as S from './Search.style';
 /* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 const Search = ({ history }) => {
-  console.log(history);
   const [value, setValue] = useState('javascript');
   const onInputChange = (event) => {
     setValue(event.target.value);
@@ -12,24 +12,23 @@ const Search = ({ history }) => {
     history.push(`/search/${value}`);
   };
   return (
-    <section>
+    <S.Wrapper>
       <h2>Find the best time for a subreddit</h2>
-      <form onSubmit={onSubmit}>
-        <div className="form-wrapper">
-          <label htmlFor="reddit">
-            r/
-            <input
-              value={value}
-              id="reddit"
-              name="reddit"
-              type="text"
-              onChange={onInputChange}
-            />
-          </label>
-          <button type="submit">Search</button>
-        </div>
-      </form>
-    </section>
+      <S.Form onSubmit={onSubmit}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <S.FormLabel htmlFor="reddit">
+          r/
+          <S.FormInput
+            value={value}
+            id="reddit"
+            name="reddit"
+            type="text"
+            onChange={onInputChange}
+          />
+        </S.FormLabel>
+        <S.FormButton type="submit">Search</S.FormButton>
+      </S.Form>
+    </S.Wrapper>
   );
 };
 
