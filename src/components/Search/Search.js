a2d070f94ef0877bb946bf5e364567870c068876
Ajea Smith <smithajea@rocketmail.com>
2021-03-21 13:52:16 -0400

    text input prefixed with r /

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Search = () => (
   <section>
     <h2>Find the best time for a subreddit</h2>
-    <form></form>
+    <form>
+      <div className="form-wrapper">
+        <label htmlFor="reddit">
+          r/
+          <input
+            id="reddit"
+            name="reddit"
+            type="text"
+            placeholder="javascript"
+          />
+        </label>
+      </div>
+    </form>
   </section>
 );
 
