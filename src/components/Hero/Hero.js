d59d734e4203d314a020700a0d45456f4a57649a
Ajea Smith <smithajea@rocketmail.com>
2021-03-24 14:16:57 -0400

    links to ooloo.io and ooloo.io/employers

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -27,36 +27,26 @@ const Hero = () => (
     </section>
     <S.Info>
       <S.InfoContainer>
-        <S.InfoTitle id="how-it-works">
-          How it works
-        </S.InfoTitle>
+        <S.InfoTitle id="how-it-works">How it works</S.InfoTitle>
         <S.InfoContent>
-          {' '}
+          {" "}
           • We find the 500 top posts from the past year for a subreddit.
-          <br />
-          {' '}
-          • The data is visualized in a heatmap grouped by weekday and
+          <br /> • The data is visualized in a heatmap grouped by weekday and
           hour of the day.
-          <br />
-          • See immediately when to submit your reddit post.
+          <br />• See immediately when to submit your reddit post.
         </S.InfoContent>
       </S.InfoContainer>
       <section>
         <S.InfoTitle id="about">About</S.InfoTitle>
         <S.InfoContent>
-          This app was created during a course on
-          {' '}
-          <a href="https://ooloo.io">profy.dev</a>
-          {' '}
-          with the goal to implement a
+          This app was created during a course on{" "}
+          <a href="https://ooloo.io">ooloo.io</a> with the goal to implement a
           pixel-perfect real-world application with professional workflows and
           tools like Kanban, ClickUp, Figma, GitHub, pull requests and code
-          reviews.
-          {' '}
+          reviews.{" "}
           <a href="https://ooloo.io/employers">
             Click here for more information.
-          </a>
-          {' '}
+          </a>{" "}
         </S.InfoContent>
       </section>
     </S.Info>
