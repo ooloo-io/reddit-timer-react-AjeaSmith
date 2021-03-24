bbbe70f687f58868d58925cadfa0b6bdbf551670
Ajea Smith <smithajea@rocketmail.com>
2021-03-24 13:05:56 -0400

    fixed eslint error

--- src/__tests__/SubredditForm.js
+++ src/__tests__/SubredditForm.js
@@ -1,45 +1,45 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { Router } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import userEvent from "@testing-library/user-event";
-import App from "../App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 const setup = (history) => {
   render(
     <Router history={history}>
       <App />
-    </Router>
+    </Router>,
   );
 };
 
-test("it renders search page on search link click", () => {
-  const history = createMemoryHistory({ initialEntries: ["/"] });
+test('it renders search page on search link click', () => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
   setup(history);
 
-  const searchLink = screen.getByRole("link", { name: /Search/i });
+  const searchLink = screen.getByRole('link', { name: /Search/i });
   userEvent.click(searchLink);
 
   expect(
-    screen.getByRole("heading", {
+    screen.getByRole('heading', {
       name: /Find the best time for a subreddit/i,
-    })
+    }),
   ).toBeInTheDocument();
 });
 
-test("input value changes URL param", () => {
-  const history = createMemoryHistory({ initialEntries: ["/"] });
+test('input value changes URL param', () => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
   setup(history);
 
-  const inputField = screen.getByRole("textbox", { name: "r/" });
-  const searchButton = screen.getByRole("button", { name: /Search/i });
-  expect(inputField).toHaveValue("javascript");
+  const inputField = screen.getByRole('textbox', { name: 'r/' });
+  const searchButton = screen.getByRole('button', { name: /Search/i });
+  expect(inputField).toHaveValue('javascript');
 
   userEvent.clear(inputField);
-  userEvent.type(inputField, "reactjs");
+  userEvent.type(inputField, 'reactjs');
 
   userEvent.click(searchButton);
 
-  history.push("/search/reactjs");
-  expect(history.location.pathname).toEqual("/search/reactjs");
+  history.push('/search/reactjs');
+  expect(history.location.pathname).toEqual('/search/reactjs');
 });
